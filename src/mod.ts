c19cef61d9ee94decdf9398245b9cbee10a31033
Satont <satontworldwide@gmail.com>
2021-11-25 21:18:56 +0300

    update

--- src/mod.ts
+++ src/mod.ts
@@ -1,71 +1,72 @@
-import { Client, StorageAdapter, buildQueryRunner } from './deps.deno.ts'
+import { Client, StorageAdapter, buildQueryRunner } from './deps.deno.ts';
 
 interface AdapterConstructor {
   client: Client;
-  tableName: string,
-  query: (query: string, params?: string[] | undefined) => Promise<any>
+  tableName: string;
+  query: (query: string, params?: string[] | undefined) => Promise<any>;
 }
 
 interface DbOject {
-  key: string,
-  value: string
+  key: string;
+  value: string;
 }
 
 export class PsqlAdapter<T> implements StorageAdapter<T> {
-  private tableName: string
-  private query = (query: string, params?: string[] | undefined): Promise<unknown> | unknown => null
+  private tableName: string;
+  private query = (query: string, params?: string[] | undefined): Promise<unknown> | unknown => null;
 
   /**
-    * @private
-  */
+   * @private
+   */
   private constructor(opts: AdapterConstructor) {
-    this.tableName = opts.tableName
-    this.query = opts.query
+    this.tableName = opts.tableName;
+    this.query = opts.query;
   }
 
   static async create(opts = { tableName: 'sessions' } as Omit<AdapterConstructor, 'query'>) {
     const queryString = `
-      CREATE TABLE IF NOT EXISTS "$1" (
+      CREATE TABLE IF NOT EXISTS "${opts.tableName}" (
         "key" VARCHAR NOT NULL,
         "value" TEXT
-      )`
-    const query = buildQueryRunner(opts.client)
-    await query(queryString, [opts.tableName])
-    await query(`CREATE UNIQUE INDEX "IDX_$1" ON "$1" ("key")`, [opts.tableName])
+      )`;
+    const query = buildQueryRunner(opts.client);
+    await query(queryString);
+    await query(`CREATE UNIQUE INDEX "IDX_$1" ON "$1" ("key")`, [opts.tableName]);
 
     return new PsqlAdapter({
       ...opts,
       query,
-    })
+    });
   }
 
   private async findSession(key: string) {
-    const results = await this.query(`select * from "$1" where key = $2`, [this.tableName, key]) as DbOject[]
-    const session = results[0]
+    const results = (await this.query(`select * from "${this.tableName}" where key = $1`, [key])) as DbOject[];
+    const session = results[0];
 
-    return session
+    return session;
   }
 
   async read(key: string) {
-    const session = await this.findSession(key)
+    const session = await this.findSession(key);
 
     if (!session) {
-      return undefined
+      return undefined;
     }
 
-    return JSON.parse(session.value as string) as T
+    return JSON.parse(session.value as string) as T;
   }
 
   async write(key: string, value: T) {
-    await this.query(`
-      INSERT INTO "$1" (key, value)
-      values ($2, $3)
-      ON CONFLICT (key) DO UPDATE SET value = $3`, 
-      [this.tableName, key, JSON.stringify(value)]
-    )
+    await this.query(
+      `
+      INSERT INTO "${this.tableName}" (key, value)
+      values ($1, $2)
+      ON CONFLICT (key) DO UPDATE SET value = $3`,
+      [key, JSON.stringify(value)]
+    );
   }
 
   async delete(key: string) {
-    await this.query(`delete from $1 where key = $2`, [this.tableName, key])
+    await this.query(`delete from ${this.tableName} where key = $1`, [key]);
   }
 }
