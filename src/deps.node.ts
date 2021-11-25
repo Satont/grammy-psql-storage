645ac760f8848609faf83668ab87f16098441fcd
Satont <satontworldwide@gmail.com>
2021-11-25 21:35:42 +0300

    Lint node deps file

--- src/deps.node.ts
+++ src/deps.node.ts
@@ -4,8 +4,8 @@ export type { Client } from 'pg';
 
 export function buildQueryRunner(client: Client) {
   return async (query: string, params?: string[]) => {
-    const { rows } = await client.query(query, params)
+    const { rows } = await client.query(query, params);
 
-    return rows
-  }
+    return rows;
+  };
 }
