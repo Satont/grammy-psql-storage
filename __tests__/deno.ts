714c0e61d0cf9c8dd08f824a34c1c1e70b353d24
Satont <satontworldwide@gmail.com>
2021-12-10 17:47:44 +0300

    Use @satont/grammy-storage-utils for tests

--- __tests__/deno.ts
+++ __tests__/deno.ts
@@ -1,20 +1,8 @@
-import { Bot, Context, SessionFlavor, session } from 'https://deno.land/x/grammy@v1.4.2/mod.ts';
+import { session } from 'https://deno.land/x/grammy@v1.4.2/mod.ts';
 import { expect } from 'https://deno.land/x/expect/mod.ts';
 import { PsqlAdapter } from '../src/mod.ts';
 import { Client } from 'https://deno.land/x/postgres@v0.14.2/mod.ts';
-
-interface SessionData {
-  pizzaCount: number;
-}
-
-interface StringSessionFlavor {
-  get session(): string;
-  set session(session: string | null | undefined);
-}
-
-Deno.test('Bot should be created', () => {
-  expect(createBot()).not.toBeFalsy();
-});
+import * as utils from 'https://deno.land/x/grammy_storage_utils@1.0.0/mod.ts'
 
 Deno.test('Pizza counter tests', async () => {
   const client = new Client({
@@ -24,7 +12,7 @@ Deno.test('Pizza counter tests', async () => {
     hostname: 'localhost',
     port: 5432,
   });
-  const bot = createBot<SessionData>();
+  const bot = utils.createBot();
 
   bot.use(
     session({
@@ -42,8 +30,8 @@ Deno.test('Pizza counter tests', async () => {
     expect(ctx.session.pizzaCount).toEqual(1);
   });
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 
   await client.end();
 });
@@ -57,17 +45,7 @@ Deno.test('Simple string tests', async () => {
     port: 5432,
   });
 
-  const bot = new Bot<Context & StringSessionFlavor>('fake-token', {
-    botInfo: {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      supports_inline_queries: false,
-    },
-  });
+  const bot = utils.createBot(false)
 
   bot.use(
     session({
@@ -86,46 +64,9 @@ Deno.test('Simple string tests', async () => {
     expect(ctx.session).toEqual('test edited');
   });
 
-  await bot.handleUpdate(createMessage(bot, 'first').update);
-  await bot.handleUpdate(createMessage(bot, 'second').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'first').update);
+  await bot.handleUpdate(utils.createMessage(bot, 'second').update);
 
   await client.end();
 });
 
-function createBot<T>(token = 'fake-token') {
-  return new Bot<Context & SessionFlavor<T>>(token, {
-    botInfo: {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      supports_inline_queries: false,
-    },
-  });
-}
-
-function createMessage(bot: Bot<any>, text = 'Test Text') {
-  const createRandomNumber = () => Math.floor(Math.random() * (123456789 - 1) + 1);
-
-  const ctx = new Context(
-    {
-      update_id: createRandomNumber(),
-      message: {
-        text,
-        message_id: createRandomNumber(),
-        chat: {
-          id: 1,
-          type: 'private',
-          first_name: 'Test User',
-        },
-        date: Date.now(),
-      },
-    },
-    bot.api,
-    bot.botInfo
-  );
-
-  return ctx;
-}
